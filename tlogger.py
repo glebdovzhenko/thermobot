226eeaf897682ce5bcd03a67b0c8f86ee50e843f
Gleb Dovzhenko <dovjenko.g@gmail.com>
2024-11-09 15:27:20 +0700

    Added statistical analysis of collected data

--- tlogger.py
+++ tlogger.py
@@ -1,6 +1,7 @@
 import asyncio
 import datetime
 import os
+import numpy as np
 
 from dbio import DBIO
 
@@ -37,12 +38,90 @@ class Thermometer:
                 self._dht.exit()
                 raise err
 
-    async def measure_infinite(self, period: int = 1):
+    async def measure_infinite(self):
         while True:
             dt = int(os.environ["MEAS_PERIOD"])
-            n_steps = os.environ["MEAS_PTS"]
-            self.measure_once()
-            await asyncio.sleep(dt)
+            n_steps = int(os.environ["MEAS_PTS"])
+
+            cpu_t, dht_t, dht_h = (
+                np.empty(n_steps),
+                np.empty(n_steps),
+                np.empty(n_steps),
+            )
+            cpu_t[:] = np.NaN
+            dht_t[:] = np.NaN
+            dht_h[:] = np.NaN
+
+            for ii in range(n_steps):
+                try:
+                    cpu_t[ii] = self.get_cpu()
+                except Exception as err:
+                    print(err)
+
+                if self._dht is not None:
+                    try:
+                        dht_t[ii] = self._dht.temperature
+                    except RuntimeError as re:
+                        print(re.args[0])
+                    except Exception as err:
+                        self._dht.exit()
+                        raise err
+                    try:
+                        dht_h[ii] = self._dht.humidity
+                    except RuntimeError as re:
+                        print(re.args[0])
+                    except Exception as err:
+                        self._dht.exit()
+                        raise err
+
+                await asyncio.sleep(dt / (n_steps - 1))
+
+            print(datetime.datetime.now())
+            if not np.all(np.isnan(cpu_t)):
+                print(
+                    "CPU: [%.01f, %.01f], %d / %d fails, %.01f ± %.01f"
+                    % (
+                        np.nanmin(cpu_t),
+                        np.nanmax(cpu_t),
+                        np.isnan(cpu_t).sum(),
+                        n_steps,
+                        np.nanmean(cpu_t),
+                        np.nanstd(cpu_t),
+                    )
+                )
+                self._dbio.append_cpu_reading(
+                    datetime.datetime.now(), np.nanmean(cpu_t), verbose=False
+                )
+
+            if (not np.all(np.isnan(dht_t))) and (not np.all(np.isnan(dht_h))):
+                print(
+                    "DHT22 T: [%.01f, %.01f], %d / %d fails, %.01f ± %.01f"
+                    % (
+                        np.nanmin(dht_t),
+                        np.nanmax(dht_t),
+                        np.isnan(dht_t).sum(),
+                        n_steps,
+                        np.nanmean(dht_t),
+                        np.nanstd(dht_t),
+                    )
+                )
+                print(
+                    "DHT22 H: [%.01f, %.01f], %d / %d fails, %.01f ± %.01f"
+                    % (
+                        np.nanmin(dht_h),
+                        np.nanmax(dht_h),
+                        np.isnan(dht_h).sum(),
+                        n_steps,
+                        np.nanmean(dht_h),
+                        np.nanstd(dht_h),
+                    )
+                )
+                self._dbio.append_dht_reading(
+                    datetime.datetime.now(),
+                    np.nanmean(dht_t),
+                    np.nanmean(dht_h),
+                    verbose=False,
+                )
 
 
 if __name__ == "__main__":
